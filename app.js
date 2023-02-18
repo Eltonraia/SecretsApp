be262b7955fe119c4611ad868d284ba49f5ad341
Eltonraia <elton14739@yahoo.com>
2023-02-17 18:56:22 -0600

    modified app.js for test

--- app.js
+++ app.js
@@ -8,6 +8,6 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(3000, function () {
+app.listen(1000, function () {
   console.log("Server started on port 3000");
 });
